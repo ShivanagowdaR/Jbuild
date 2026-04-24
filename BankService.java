3830106c4988cba4857398c6dd880e43c88dfb20
ShivanagowdaR <rshivanagowda22@gmail.com>
2026-04-24 13:00:11 +0530

    Create BankService.java

---
+++ BankService.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class BankService {
+  static class BankAccount {
+    double balance;
+  }
+  double balance{
+    balance=money;
+  }
+  void deposit(double money){
+    balance = balance + money;
+  }
+  void withdraw(double money){
+    balance = balance - money;
+  }
+  double getbalance(){
+    balance = money;
+  }
+
+public static void main(String[] args){
+  BankAccount acc = new BankAccount(5000);
+  System.out.println("initial balance : ",+acc.getbalance());
+  acc.deposit(600);
+   System.out.println("After deposite : ",+acc.getbalance());
+  acc.withdraw(200);
+   System.out.println("After withdraw : ",+acc.getbalance());
+  acc.getbalance();
+   System.out.println("Final balance : ",+acc.getbalance());
+}
+}
+
