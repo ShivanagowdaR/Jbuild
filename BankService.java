b0bd85064feb4029d46430a6a4b0ec1058a28449
ShivanagowdaR <rshivanagowda22@gmail.com>
2026-04-24 13:29:59 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -1,29 +1,39 @@
-import java.util.*;
-public class BankService {
-  static class BankAccount {
-    double balance;
-  
-  BankAccount{
-    double money;
-  }
-  void deposit(double money){
-    balance = balance + money;
-  }
-  void withdraw(double money){
-    balance = balance - money;
-  }
-  double getbalance(){
-    balance = money;
-  }
-  }
-public static void main(String[] args){
-  BankAccount acc = new BankAccount(5000);
-  System.out.println("initial balance : ",+acc.getbalance());
-  acc.deposit(600);
-   System.out.println("After deposite : ",+acc.getbalance());
-  acc.withdraw(200);
-   System.out.println("After withdraw : ",+acc.getbalance());
-  System.out.println("Final balance : ",+acc.getbalance());
-}
-}
+public class BankService {    
+    static class BankAccount {
+        double balance;
+        BankAccount(double money) {
+            balance = money;
+        }
 
+        
+        void deposit(double money) {
+            balance = balance + money;
+        }
+
+        
+        void withdraw(double money) {
+            balance = balance - money;
+        }
+
+        
+        double getBalance() {
+            return balance;
+        }
+    }
+
+    
+    public static void main(String[] args) {
+
+        BankAccount acc = new BankAccount(5000);
+
+        System.out.println("Initial Balance: " + acc.getBalance());
+
+        acc.deposit(600);
+        System.out.println("After Deposit: " + acc.getBalance());
+
+        acc.withdraw(200);
+        System.out.println("After Withdraw: " + acc.getBalance());
+
+        System.out.println("Final Balance: " + acc.getBalance());
+    }
+}
